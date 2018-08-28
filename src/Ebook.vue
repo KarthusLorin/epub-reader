8e48838863a738e0a82b7d318a1e4834124afc57
KarthusLorin <karthuslorin@gmail.com>
2018-08-28 22:56:34 +0800

    add ebook

---
+++ src/Ebook.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+global.ePub = Epub
+export default {
+  methods: {
+    // 电子书的解析和渲染
+    showEpub () {
+      // 生成Book
+      this.book = new Epub(DOWNLOAD_URL)
+      // 生成Rendition
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 通过Rendtion.display渲染电子书
+      this.rendition.display()
+    }
+  },
+  mounted () {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "assets/styles/global";
+</style>
